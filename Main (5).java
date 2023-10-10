fc9c34f5b977cd95b8a9be4c786843231957be62
akshaycsit <143694487+akshaycsit@users.noreply.github.com>
2023-10-10 13:03:33 +0530

    Add files via upload

--- Main (5).java
+++ Main (5).java
@@ -1,19 +1,94 @@
-import java.util.Scanner;
-public class Main{
- public static void main(String args[]){
- System.out.println("Enter any alphabet: ");
- Scanner sc=new Scanner(System.in);
- char a=sc.next().charAt(0);
- if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'){
- System.out.println("The entered alphabet is a vowel.");
- }
- else if(a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
- System.out.println("The entered alphabet is a vowel.");
- }
- else{
- System.out.println("The entered alphabet is a consonant.");
- }
- 
-}
+/*
+class Parent {
+    void display() {
+        System.out.println("This is the parent class");
+    }
+
+    void show(int num) {
+        System.out.println("The number in the parent class is: " + num);
+    }
 }
 
+class Child extends Parent {
+    @Override
+    void display() {
+        System.out.println("This is the child class");
+    }
+
+    void show(double num) {
+        System.out.println("The number in the child class is: " + num);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Parent parentObj = new Parent();
+        Parent childObj = new Child();
+        
+        parentObj.show(5);    
+        childObj.show(7);   
+
+        
+        parentObj.display();  
+        childObj.display();   
+    }
+}*/
+
+class Shape {
+    public double area() {
+        return 0.0; // Default implementation for area
+    }
+    
+    public double perimeter() {
+        return 0.0; // Default implementation for perimeter
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius; // Override area for circle
+    }
+
+    
+    public double perimeter() {
+        return 2 * Math.PI * radius; // Override perimeter for circle
+    }
+}
+
+
+class Square extends Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+  
+    public double area() {
+        return side * side; 
+    }
+
+  
+    public double perimeter() {
+        return 4 * side; 
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Square square = new Square(4.0);
+
+        System.out.println("Circle Area: " + circle.area());
+        System.out.println("Circle Perimeter: " + circle.perimeter());
+
+        System.out.println("Square Area: " + square.area());
+        System.out.println("Square Perimeter: " + square.perimeter());
+    }
+}
