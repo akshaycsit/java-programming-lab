b807e8d9a41cb0504c84ea6cbd1656fd13568e82
akshaycsit <143694487+akshaycsit@users.noreply.github.com>
2023-10-10 12:51:42 +0530

    Add files via upload

--- Main (4).java
+++ Main (4).java
@@ -1,15 +1,35 @@
-import java.util.Scanner;
-public class Main{
- public static void main(String args[]){
 
- System.out.println("Enter any two numbers");
- Scanner sc=new Scanner(System.in);
- int a=sc.nextInt();
- int b=sc.nextInt();
- int c;
- c=a;
- a=b;
- b=c;
- System.out.println("The swapped numbers are:"+a+" "+"and"+" "+b);
- }
+class Parent {
+    void display() {
+        System.out.println("This is the parent class");
+    }
+
+    void show(int num) {
+        System.out.println("The number in the parent class is: " + num);
+    }
+}
+
+class Child extends Parent {
+    @Override
+    void display() {
+        System.out.println("This is the child class");
+    }
+
+    void show(double num) {
+        System.out.println("The number in the child class is: " + num);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Parent parentObj = new Parent();
+        Parent childObj = new Child();
+        
+        parentObj.show(5);    
+        childObj.show(7);   
+
+        
+        parentObj.display();  
+        childObj.display();   
+    }
 }
